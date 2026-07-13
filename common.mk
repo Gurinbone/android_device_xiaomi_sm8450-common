9c498b1435db3b5448cf9252b28c3427de1b8bad
gurinbone <gurinbone@gmail.com>
2026-07-13 05:26:21 +0000

    Dolby Spatial & Bluetooth

--- common.mk
+++ common.mk
@@ -99,6 +99,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration_7_0.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
@@ -314,10 +315,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Parts
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
@@ -501,27 +498,3 @@ PRODUCT_PACKAGES += \
     firmware_qca6490_wlan_mac.bin_symlink \
     firmware_qca6750_WCNSS_qcom_cfg.ini_symlink \
     firmware_qca6750_wlan_mac.bin_symlink
-
-# Dolby Vision Flag
-TARGET_INCLUDES_DolbyVision := true
-
-# LunarisDolby - use
-PRODUCT_PACKAGES += \
-    LunarisDolby
-
-# Dolby
-$(call inherit-product-if-exists, hardware/dolby/dolby.mk)
-
-# MiuiCam
-$(call inherit-product-if-exists, device/xiaomi/miuicamera-ingres/device.mk)
-
-# GameKeys
-PRODUCT_PACKAGES += \
-    vendor.lineage.gamekeys-service.default \
-    vendor.lineage.touchinjector-service.default \
-    GameKeys
-
-# Leds
-PRODUCT_PACKAGES += \
-    vendor.lineage.leds-service.default \
-    Leds
